019b34d043f0d3a6e3150c19241f1749eaf561a5
Suyogya Luitel <lsuyogya@gmail.com>
2024-08-05 12:51:40 +0545

    form messages

--- app/services/[serviceName]/page.tsx
+++ app/services/[serviceName]/page.tsx
@@ -6,6 +6,8 @@ import style from '@/app/_styles/serviceDetail.module.scss';
 import { SubmitButton } from '@/app/_components/SubmitButton';
 import ClientTestimonials from '@/app/_components/ClientTestimonials';
 import MiniServices from '@/app/_components/MiniServices';
+import serviceDetailFormSubmit from '@/app/actions/serviceDetailFormSubmit';
+import ServiceDetailForm from './ServiceDetailForm';
 
 const getData = async (serviceName: string) => {
   const res = await fetch(`${process.env.baseUrl}/services/${serviceName}`);
@@ -15,38 +17,7 @@ const getData = async (serviceName: string) => {
 
 const Page = async ({ params }: { params: { serviceName: string } }) => {
   const data = await getData(params.serviceName);
-  async function formAction(serviceName: string, formData: FormData) {
-    'use server';
-    console.log(serviceName);
-    const rawFormData = {
-      full_name: formData.get('name'),
-      email_address: formData.get('email'),
-      contact_no: formData.get('contactNo'),
-      message: formData.get('message'),
-      service_name: serviceName,
-    };
 
-    try {
-      const response = await fetch(`${process.env.baseUrl}/service-enquiry`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(rawFormData),
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      const result = await response.json();
-      console.log('Success:', result);
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  }
-
-  const formActionWithService = formAction.bind(null, params.serviceName);
   return (
     <>
       <Banner
@@ -58,14 +29,14 @@ const Page = async ({ params }: { params: { serviceName: string } }) => {
           <div className={`${style.contentWrapper}`}>
             <div className={`${style.leftContent}`}>
               <h1> {data.description_title} </h1>
-              {data.description_content.map(
+              {data.description_content?.map(
                 (contentParagraph: string, index: number) => (
                   <p key={index}> {contentParagraph}</p>
                 )
               )}
               <h2> {data.expectation_title} </h2>
               <ul>
-                {data.expectation_content.map(
+                {data.expectation_content?.map(
                   (listContent: { description: string }, index: number) => (
                     <li
                       key={index}
@@ -79,45 +50,7 @@ const Page = async ({ params }: { params: { serviceName: string } }) => {
             </div>
             <div className={style.rightWrapper}>
               <div className={`${style.rightContent}`}>
-                <form action={formActionWithService}>
-                  <div className="formGroup">
-                    <label htmlFor="name">Name</label>
-                    <input
-                      id="name"
-                      name="name"
-                      placeholder="Name"
-                    />
-                  </div>
-                  <div className="formGroup">
-                    <label htmlFor="email">Email</label>
-                    <input
-                      id="email"
-                      name="email"
-                      placeholder="Email"
-                    />
-                  </div>
-                  <div className="formGroup">
-                    <label htmlFor="contactNo">Contact Number</label>
-                    <input
-                      id="contactNo"
-                      name="contactNo"
-                      placeholder="Contact No"
-                      required
-                    />
-                  </div>
-                  <div className="formGroup">
-                    <label htmlFor="message">Message</label>
-                    <textarea
-                      id="message"
-                      name="message"
-                      placeholder="Message"
-                      rows={5}
-                    />
-                  </div>
-                  <div className="formGroup">
-                    <SubmitButton btnLabel="Enquire Now" />
-                  </div>
-                </form>
+                <ServiceDetailForm serviceName={params.serviceName} />
               </div>
             </div>
           </div>
